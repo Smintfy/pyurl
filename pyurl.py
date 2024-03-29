7cdb07643b5e7b1ed21384cb5d50c6ac19e674f8
Smintfy <hey@smintfy.com>
2024-03-29 18:59:29 +0700

    (update): improvement

--- pyurl.py
+++ pyurl.py
@@ -5,65 +5,80 @@ import argparse
 import validators
 from urllib.parse import urlparse
 
-def encode_request(method, host, path, data, header):
-    # default to GET method if method not specified
-    if not method:
-        method = "GET"
-    
-    # default data to empty string
-    data = data or ""
+# protocol
+HTTP = "http"
+HTTPS = "https"
 
-    req = f"{method} {path} HTTP/1.1\r\n"
-    req += f"Host:{host}\r\n"
+# localhost
+LOCALHOST = "localhost"
 
-    match method:
-        case "GET" | "DELETE":
-            req += "Accept: */*\r\n"
-            req += "Connection: close\r\n\r\n"
-            return req.encode()
-        case "POST" | "PUT" | "PATCH":
-            req += f"{header}\r\n"
-            req += f"Content-Length: {len(data)}\r\n"
-            req += f"Connection: close\r\n\r\n{data}" # request body
-            return req.encode()
-        case "HEAD":
-            req += "Connection: close\r\n\r\n"
-            return req.encode()
-        case _:
-            print("Error: Method doesn't exist")
+class Sock:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, host: str, protocol: str, port: int | None, verbose=False) -> None:
+        # default port
+        if port is None:
+            if port is HTTPS:
+                port = 443
+            else:
+                port = 80
+
+        try:
+            # create a TCP connection
+            self.sock.connect((host, port))
+
+            # wrap the connection with SSL context if protocol is HTTPS
+            if protocol is HTTPS:
+                context = ssl.create_default_context()
+                self.sock = context.wrap_socket(self.sock, server_hostname=host)
+        except:
+            print(f'Error: Failed to connect to {host} port {port}: Connection refused')
             sys.exit(1)
-        
-def handle_request(req, host, protocol, port):
-    if not port:
-        if protocol == "https":
-            port = 443
-        else:
-            port = 80
 
-    try:
-        # creates a TCP connection
-        sock = socket.create_connection((host, port))
+        if verbose:
+            print(f"* Connected to: {host} port {port}")
 
-        # wrap the socket with ssl to support HTTPS
-        if protocol == "https":
-            context = ssl.create_default_context()
-            sock = context.wrap_socket(sock, server_hostname=host)
+    def send(self, msg: str, verbose=False) -> None:
+        if verbose:
+            for header in msg.decode().split("\r\n")[:-1]:
+                print(">", header)
 
-        # send the request
-        sock.sendall(req)
+        self.sock.sendall(msg)
 
-        # read the response data from the server
+    def receive(self) -> str:
         res = ""
         while True:
-            data = sock.recv(1024)
+            data = self.sock.recv(2048)
             if not data:
                 break
             res += data.decode()
         return res
-    except ValueError as e:
-        print(e)
-        sys.exit(1)
-        
+
+def encode_request(method: str | None, host: str, path: str, data: None, header: None) -> bytes:
+    # default to GET
+    if method is None:
+        method = "GET"
+
+    # building the request
+    req = f"{method} {path} HTTP/1.1\r\n"
+    req += f"Host:{host}\r\n"
+    req += "Accept: */*\r\n"
+    req += "Connection: close\r\n"
+
+    # TODO accept more than one header
+    if header:
+        req += f"{header}\r\n"
+
+    # request ends with \r\n\r\n
+    if data:
+        req += f"Content-Length: {len(data)}\r\n"
+        req += f"\r\n{data}" # request body
+    else:
+        req += f"\r\n"
+
+    return req.encode()
+
 def main():
     parser = argparse.ArgumentParser(prog="curlpy", usage="python main.py [options...] <url>")
     parser.add_argument("-v", action="store_true", 
@@ -79,37 +94,32 @@ def main():
     parsed_url = urlparse(url)
     host, path, protocol, port = parsed_url.hostname, parsed_url.path, parsed_url.scheme, parsed_url.port
 
-    # validate the url and localhost
-    if not validators.url(url) and not host == "localhost":
+    # validate url while also allowing localhost
+    if url is LOCALHOST and not validators.url(url):
         print('Error: provided url is not valid')
         sys.exit(1)
 
-    # only support http and https for now
-    if not protocol.startswith("http"):
+    # only support HTTP and HTTPS
+    if not protocol.startswith(HTTP):
         print('Error: protocol not supported. Only support HTTP and HTTPS')
         sys.exit(1)
 
-    # make request with method, host name, and path
+    # initiate socket
+    sock = Sock()
+    sock.connect(host, protocol, port, verbose)
+
     req = encode_request(method, host, path, data, header)
+    sock.send(req, verbose)
 
-    # print out the request before sending to the server if verbose flag specified
-    if verbose:
-        for msg in req.decode().split("\r\n"):
-            print(">", msg)
-
-    res = handle_request(req, host, protocol, port)
+    res = sock.receive()
 
     # verbose for incoming response
     if verbose:
-        for msg in res.split("\r\n"):
-            if not msg.startswith("{"):
-                print("<", msg)
-            else:
-                print(msg)
-        sys.exit(1)
+        for msg in res.split("\r\n")[:-1]:
+            print("<", msg)
 
     # default    
-    print(res)
+    print(res.split("\r\n")[-1])
 
 if __name__ == "__main__":
     main()
